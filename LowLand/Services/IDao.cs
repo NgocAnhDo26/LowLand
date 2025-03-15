ad67af7d334999bca1d97a26b7450950bdf57118
NgocAnhDo26 <dotanngocanh@gmail.com>
2025-03-15 17:20:14 +0700

    Add project files.

---
+++ LowLand/Services/IDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LowLand.Model;
+
+namespace LowLand.Services
+{
+    public interface IDao
+    {
+        IRepository<Drink> Drinks { get; set; }
+    }
+}
